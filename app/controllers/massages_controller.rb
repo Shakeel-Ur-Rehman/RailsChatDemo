8a613cd9611995cde07e1c9a80b9dbbf6721ef71
shakeel <shakeel.rehman@pikessoft.com>
2019-06-13 14:13:26 +0500

    updated controller

--- app/controllers/massages_controller.rb
+++ app/controllers/massages_controller.rb
@@ -13,34 +13,32 @@ class MassagesController < ApplicationController
         @messages=@thread.messages
         else
             @messages=[]
-        end
-        
-      
+        end  
     end
-    def sendsms
-       if @client.messages.create(
+    def sendsms   
+        if @client.messages.create(
             from:@twilio_number,
-            to:"+923015067211",
+         to:"+923015067211",
          body:params["message"]["body"]
        )
+    end
         @message=Message.new
         @message.to="+923015067211"
         @message.from=@twilio_number
-        @message.Messagethread_id=params["message"]["thread_id"]
+        @message.messagethread_id=params["message"]["thread_id"]
         @message.body=params["message"]["body"]
         @message.save
-    end
         redirect_to controller: "massages", action: 'create',thread_id: params["message"]["thread_id"]
     end
 
     def recievesms
         phonenumber=params["From"];
         @thread=Messagethread.where(phone:phonenumber);
-        if @thread.length >0
+        if @thread.length > 0
             message=Message.new
             message.to=@twilio_number
             message.from=params["From"]
-            message.Messagethread_id=@thread.id
+            message.messagethread_id=@thread.id
             message.body=params["Body"]
             message.save
         else
@@ -52,7 +50,7 @@ class MassagesController < ApplicationController
             message=Message.new
             message.to=@twilio_number
             message.from=params["From"]
-            message.Messagethread_id=@thread.id
+            message.messagethread_id=@thread.id
             message.body=params["Body"]
             message.save
         end
