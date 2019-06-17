06ed728eb81ef0c8bd41b9b0c1e5922a7f227cd9
shakeel <shakeel.rehman@pikessoft.com>
2019-06-17 19:40:07 +0500

    created a class method for saving message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ApplicationRecord
     belongs_to :messagethread
+
+    def self.create_message(to,from,body,thread_id)
+    Message.create(to:to,from:from,body:body,messagethread_id:thread_id)
+    end
 end
