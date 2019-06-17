c6c9bacd946c5ca346285c65d9cd521f52b812c1
shakeel <shakeel.rehman@pikessoft.com>
2019-06-17 16:01:26 +0500

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "sms",to:"messages#create",as:"create"
-  post "sendsms",to:"messages#sendsms",as:"sendsms"
+  post "create",to:"messages#create",as:"sendsms"
   post "sms/reply",to:"messages#recievesms"
   root "messages#index"
 end
