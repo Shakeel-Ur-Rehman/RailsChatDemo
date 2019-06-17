328db725d6915acfc1fc9f20c3c1f8e70de45025
shakeel <shakeel.rehman@pikessoft.com>
2019-06-17 19:37:58 +0500

    updated messagecontroller made class method for saving message and created private methods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,65 +6,52 @@ class MessagesController < ApplicationController
     def index
         @threads=Messagethread.order('updated_at DESC').all    #TODO Need to add some pagination here.
         if(params.has_key?(:thread_id))
-            @thread_id=params["thread_id"]
             @thread=Messagethread.find(params["thread_id"])
-            @messages=@thread.messages
         else
-            @messages=[]
+            @thread=Messagethread.last
         end  
     end
+    
     def create
-        sendsms
-        @thread=Messagethread.find(params["message"]["thread_id"])
-        @messages=@thread.messages
-    end
-    def recievesms
-        @thread=Messagethread.where(phone:params["From"]);
-        if @thread.length > 0
-            message=Message.new
-            message.to=@twilio_number
-            message.from=params["From"]
-            message.messagethread_id=@thread.id
-            message.body=params["Body"]
-            message.save
+        if(params.has_key?(:message))
+            sendsms
         else
-            @thread=Messagethread.new
-            @thread.description=params["Body"]
-            @thread.phone=params["From"]
-            @thread.topic="Thread Topic"
-            @thread.save
-            message=Message.new
-            message.to=@twilio_number
-            message.from=params["From"]
-            message.messagethread_id=@thread.id
-            message.body=params["Body"]
-            message.save
+            recievesms
         end
-   
+        @thread=Messagethread.find(params["message"]["thread_id"])
     end
 
+
 private
+
 def setup_twilio_number
     @twilio_number=ENV['twilio_number']
 end
+
 def setuptwilio
     account_sid=ENV['account_sid']
     auth_token=ENV['auth_token']
     @client=Twilio::REST::Client.new(account_sid, auth_token);
 end
 
+def recievesms
+    @thread=Messagethread.where(phone:params["From"]);
+    if @thread.length > 0 
+        Message.create_message(@twilio_number,params["From"],params["Body"],@thread.id)
+    else
+        @thread=Messagethread.create(:topic=>"Thread Topic",:phone=>params["From"],:description=>params["Body"])
+        @thread.messages.create(:to=>@twilio_number,:from=>params["From"],:body=>params["Body"])
+    end
+
+end
+
 def sendsms   
     if @client.messages.create(
-        from:@twilio_number,
-     to:"+923015067211",
+     from:@twilio_number,
+     to:params["message"]["to"],
      body:params["message"]["body"]
    )
-    @message=Message.new
-    @message.to="+923015067211"
-    @message.from=@twilio_number
-    @message.messagethread_id=params["message"]["thread_id"]
-    @message.body=params["message"]["body"]
-    @message.save
+   Message.create_message(params["message"]["to"],@twilio_number,params["message"]["body"],params["message"]["thread_id"])
    end
 end
 end
