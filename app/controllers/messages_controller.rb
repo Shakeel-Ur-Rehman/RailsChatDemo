eb297a1d11ece9caffc2af8f8180e2c84234671f
shakeel <shakeel.rehman@pikessoft.com>
2019-06-19 15:19:21 +0500

    converted if condition to ternary condition and let the message to be saved in create method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,19 @@
 class MessagesController < ApplicationController
+    require 'twilio'
     protect_from_forgery except: :recievesms
     skip_before_action :verify_authenticity_token,only:[:recievesms]
     before_action :authenticate_twilio_request,only:[:recievesms]
     before_action :setup_twilio_number
     def index
         @threads=Messagethread.order('updated_at DESC').all    #TODO Need to add some pagination here.
-        if(params.has_key?(:thread_id))
-            @thread=Messagethread.find(params["thread_id"])
-        else
-            @thread=Messagethread.last
+        if(params.has_key?(:thread_id))?@thread=Messagethread.find(params["thread_id"]):@thread=Messagethread.last
         end
     end
 
     def create
         if(params.has_key?(:message))
-            TwilioAPI.new.sendSms(params["message"]["to"],params["message"]["body"],params["message"]["thread_id"])
+            TwilioAPI.new.sendSms(params["message"]["to"],params["message"]["body"])
+            Message.create_message(params["message"]["to"],@twilio_number,params["message"]["body"],params["message"]["thread_id"])
             @thread=Messagethread.find(params["message"]["thread_id"])
         end
     end
