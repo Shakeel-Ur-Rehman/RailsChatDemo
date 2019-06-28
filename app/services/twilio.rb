787f5b4fb41bbea57dd6916c480e65c1bd0e7fa6
shakeel <shakeel.rehman@pikessoft.com>
2019-06-28 19:15:24 +0500

    updated twilio service class

--- app/services/twilio.rb
+++ app/services/twilio.rb
@@ -1,12 +1,12 @@
 class TwilioAPI
-    attr_accessor :client,:account_sid
+    attr_accessor :client
     def initialize
         account_sid=ENV['account_sid']
         auth_token=ENV['auth_token']
         @client=Twilio::REST::Client.new(account_sid, auth_token);
     end
-    def validate_twilio_request?(accountid)
-        return account_sid==accountid
+    def validate_twilio_request?(accountid) 
+        return client.account_sid===accountid
     end
     def sendSms(to,body)
         client.messages.create(
